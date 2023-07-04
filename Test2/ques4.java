f0b7dc0c09ccfff3998b6a0dbe0dd36467b6c172
amit kumar <76870053+Amit2508@users.noreply.github.com>
2023-07-04 10:19:34 +0530

    Create ques4.java

---
+++ Test2/ques4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // taking input from user
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        // Find the largest number using nested if-else statements
+        int largest;
+
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                largest = num1;
+            } else {
+                largest = num3;
+            }
+        } else {
+            if (num2 >= num3) {
+                largest = num2;
+            } else {
+                largest = num3;
+            }
+        }
+
+        // Print the largest number
+        System.out.println("The largest number is: " + largest);
+
+       
+    }
+}
