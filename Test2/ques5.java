6d24f34a36b270f2dc272517f32a96a3ca379503
amit kumar <76870053+Amit2508@users.noreply.github.com>
2023-07-04 10:21:07 +0530

    Create ques5.java

---
+++ Test2/ques5.java
@@ -0,0 +1,13 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        // Declaring and initializing variables of different data types as mentioned in ques
+        int age = 22;
+        double salary = 1000.50;
+        String name = "Amit kumar";
+
+        // Print the values of the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
